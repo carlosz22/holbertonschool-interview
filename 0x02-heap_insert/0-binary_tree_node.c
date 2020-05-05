607ba6a2bb39aaad43144b69dc6ea1619f27e3b6
Carlos Zuluaga <carlosez9@hotmail.com>
2020-05-05 13:30:17 -0500

    Add exercise 0

---
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - Creates a binary tree node
+ * @parent: is a pointer to the parent node
+ * @value: is the value to put in the new node
+ *
+ * Return: Always 0 (Success)
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node;
+
+	if (parent == NULL)
+		return (NULL);
+
+	new_node = malloc(sizeof(binary_tree_t));
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->parent = parent;
+	new_node->n = value;
+	new_node->left = NULL;
+	new_node->right = NULL;
+
+	return (new_node);
+}
---
+++ 0x02-heap_insert/README.md
@@ -0,0 +1,27 @@
+# Project: Heap Insert
+## 0x02-heap_insert
+
+## Problem Statement
+
+### 0. New node
+
+This function creates a binary tree node.
+
+When created, a node does not have any children.
+
+The function returns a pointer to the new node, or NULL on failure.
+
+### 1. Heap - Insert
+
+This function inserts a value into a Max Binary Heap.
+
+The function writes a pointer to the inserted node, or NULL on failure.
+
+If the address stored in root is NULL, the created node becomes the root node.
+
+It respects a Max Heap ordering.
+
+
+## Authors
+
+- Carlos Zuluaga  (@carlosz22)
