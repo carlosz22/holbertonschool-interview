0344b006732288ae2e2a978fa72481ab959db7a0
Carlos Zuluaga <carlosez9@hotmail.com>
2020-09-03 00:10:39 -0500

    Add log parsing exercise

---
+++ 0x06-log_parsing/0-stats.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""Parses a log stream by line"""
+
+import sys
+
+dict_codes = {'200': 0, '301': 0, '400': 0, '401': 0,
+                '403': 0, '404': 0, '405': 0, '500': 0}
+total_size = 0
+num_lines = 0
+
+try:
+    for line in sys.stdin:
+        line_list = line.split(" ")
+
+        if len(line_list) > 2:
+            size = line_list[-1]
+            status_code = line_list[-2]
+
+            if status_code in dict_codes.keys():
+                dict_codes[status_code] += 1
+
+            counter += 1
+            total_size += int(size)
+
+        if counter == 10:
+            print("File size: {}".format(total_size))
+
+            for key, value in sorted(dict_codes.items()):
+                if value > 0:
+                    print("{}: {}".format(key, value))
+            counter = 0
+
+except Exception:
+    pass
+
+finally:
+    print("File size: {}".format(total_size))
+
+    for key, value in sorted(dict_codes.items()):
+        if value > 0:
+            print("{}: {}".format(key, value))
---
+++ 0x06-log_parsing/README.md
@@ -0,0 +1,3 @@
+## Readme
+
+This is the readme for 0x06-log_parsing project
