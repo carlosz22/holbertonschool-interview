2bd13089daceb1abd20174dd9cb5a66f6ba49050
Carlos Zuluaga <carlosez9@hotmail.com>
2020-09-03 00:13:53 -0500

    Fix log parsing exercise

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -4,7 +4,7 @@
 import sys
 
 dict_codes = {'200': 0, '301': 0, '400': 0, '401': 0,
-                '403': 0, '404': 0, '405': 0, '500': 0}
+              '403': 0, '404': 0, '405': 0, '500': 0}
 total_size = 0
 num_lines = 0
 
@@ -19,16 +19,16 @@ try:
             if status_code in dict_codes.keys():
                 dict_codes[status_code] += 1
 
-            counter += 1
+            num_lines += 1
             total_size += int(size)
 
-        if counter == 10:
+        if num_lines == 10:
             print("File size: {}".format(total_size))
 
             for key, value in sorted(dict_codes.items()):
                 if value > 0:
                     print("{}: {}".format(key, value))
-            counter = 0
+            num_lines = 0
 
 except Exception:
     pass
