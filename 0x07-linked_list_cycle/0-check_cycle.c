537768e2c59c9a7d8caaca201437555e5fec7261
Carlos Zuluaga <carlosez9@hotmail.com>
2020-09-06 20:36:58 -0500

    Add check cycle exercise

---
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * check_cycle - prints all elements of a listint_t list
+ * @list: pointer to head of list
+ * Return: 1 if has a cycle, 0 otherwise
+ */
+
+int check_cycle(listint_t *list)
+{
+    listint_t *head;
+    listint_t *current;
+
+    if (list == NULL)
+        return 0;
+
+    head = list;
+    current = list;
+
+    while (current != NULL)
+    {
+        if (current->next == head)
+        {
+            return 1;
+        }
+        current = current->next;
+    }
+    return 0;
+}
