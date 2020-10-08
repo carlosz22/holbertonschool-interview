8094bf13eeb7f457b3f82ca3d9b5024053f2d5ca
Carlos Zuluaga <carlosez9@hotmail.com>
2020-10-07 23:22:01 -0500

    Fix betty style

--- 0x0B-menger/0-menger.c
+++ 0x0B-menger/0-menger.c
@@ -10,20 +10,20 @@
  */
 void rotate(char **p, int S)
 {
-    int i, j, tmp;
+	int i, j, tmp;
 
-    for (i = 0; i < S / 2; i++)
-    {
-        for (j = i; j < S - i - 1; j++)
-        {
+	for (i = 0; i < S / 2; i++)
+	{
+		for (j = i; j < S - i - 1; j++)
+		{
 
-            tmp = p[i][j];
-            p[i][j] = p[S - 1 - j][i];
-            p[S - 1 - j][i] = p[S - 1 - i][S - 1 - j];
-            p[S - 1 - i][S - 1 - j] = p[j][S - 1 - i];
-            p[j][S - 1 - i] = tmp;
-        }
-    }
+			tmp = p[i][j];
+			p[i][j] = p[S - 1 - j][i];
+			p[S - 1 - j][i] = p[S - 1 - i][S - 1 - j];
+			p[S - 1 - i][S - 1 - j] = p[j][S - 1 - i];
+			p[j][S - 1 - i] = tmp;
+		}
+	}
 }
 /**
  * finallyfree - free memory
@@ -33,13 +33,13 @@ void rotate(char **p, int S)
 
 void finallyfree(char **p, int i)
 {
-    int j;
+	int j;
 
-    for (j = 0; j < i; j++)
-    {
-        free(p[j]);
-    }
-    free(p);
+	for (j = 0; j < i; j++)
+	{
+		free(p[j]);
+	}
+	free(p);
 }
 /**
  * alloc_grid - concatenates two strings
@@ -50,30 +50,30 @@ void finallyfree(char **p, int i)
  */
 char **alloc_grid(int width, int height)
 {
-    char **p;
-    int i, j;
+	char **p;
+	int i, j;
 
-    if (width > 0 && height > 0)
-    {
-        p = (char **)malloc(sizeof(char *) * height);
-        if (p == NULL)
-            return (NULL);
-        for (i = 0; i < height; i++)
-        {
-            p[i] = (char *)malloc((width) * sizeof(char));
-            if (p[i] == NULL)
-            {
-                finallyfree(p, i);
-                return (0);
-            }
-            for (j = 0; j < width; j++)
-            {
-                p[i][j] = 35;
-            }
-        }
-        return (p);
-    }
-    return (NULL);
+	if (width > 0 && height > 0)
+	{
+		p = (char **)malloc(sizeof(char *) * height);
+		if (p == NULL)
+			return (NULL);
+		for (i = 0; i < height; i++)
+		{
+			p[i] = (char *)malloc((width) * sizeof(char));
+			if (p[i] == NULL)
+			{
+				finallyfree(p, i);
+				return (0);
+			}
+			for (j = 0; j < width; j++)
+			{
+				p[i][j] = 35;
+			}
+		}
+		return (p);
+	}
+	return (NULL);
 }
 /**
  * hole_maker - makes a hole in the middle
@@ -86,30 +86,30 @@ char **alloc_grid(int width, int height)
  */
 char **hole_maker(char **p, int size, char **p_test, int size_test)
 {
-    int hole, i, j, k;
+	int hole, i, j, k;
 
-    hole = size / 3;
+	hole = size / 3;
 
-    for (i = hole; i < 2 * hole; i++)
-    {
-        for (j = hole; j < 2 * hole; j++)
-            p[j][i] = 32;
-    }
-    if (size > 1)
-    {
-        for (k = 0; k < 3; k++)
-        {
-            hole_maker(p + k * (size / 3), size / 3, p_test, size_test);
-            rotate(p + k * (size / 3), size / 3);
-            hole_maker(p + k * (size / 3), size / 3, p_test, size_test);
-            rotate(p + k * (size / 3), size / 3);
-            hole_maker(p + k * (size / 3), size / 3, p_test, size_test);
-            rotate(p + k * (size / 3), size / 3);
-            hole_maker(p + k * (size / 3), size / 3, p_test, size_test);
-            rotate(p + k * (size / 3), size / 3);
-        }
-    }
-    return (p);
+	for (i = hole; i < 2 * hole; i++)
+	{
+		for (j = hole; j < 2 * hole; j++)
+			p[j][i] = 32;
+	}
+	if (size > 1)
+	{
+		for (k = 0; k < 3; k++)
+		{
+			hole_maker(p + k * (size / 3), size / 3, p_test, size_test);
+			rotate(p + k * (size / 3), size / 3);
+			hole_maker(p + k * (size / 3), size / 3, p_test, size_test);
+			rotate(p + k * (size / 3), size / 3);
+			hole_maker(p + k * (size / 3), size / 3, p_test, size_test);
+			rotate(p + k * (size / 3), size / 3);
+			hole_maker(p + k * (size / 3), size / 3, p_test, size_test);
+			rotate(p + k * (size / 3), size / 3);
+		}
+	}
+	return (p);
 }
 /**
  * menger - draws a 2D Menger Sponge
@@ -117,33 +117,33 @@ char **hole_maker(char **p, int size, char **p_test, int size_test)
  */
 void menger(int level)
 {
-    int size, i, j;
-    char **p;
+	int size, i, j;
+	char **p;
 
-    if (level < 0)
-        exit(0);
-    if (level == 0)
-    {
-        printf("#\n");
-        exit(0);
-    }
-    size = pow(3, level);
-    p = alloc_grid(size, size);
-    hole_maker(p, size, p, size);
-    rotate(p, size);
-    hole_maker(p, size, p, size);
-    rotate(p, size);
-    hole_maker(p, size, p, size);
-    rotate(p, size);
-    hole_maker(p, size, p, size);
-    rotate(p, size);
-    hole_maker(p, size, p, size);
+	if (level < 0)
+		exit(0);
+	if (level == 0)
+	{
+		printf("#\n");
+		exit(0);
+	}
+	size = pow(3, level);
+	p = alloc_grid(size, size);
+	hole_maker(p, size, p, size);
+	rotate(p, size);
+	hole_maker(p, size, p, size);
+	rotate(p, size);
+	hole_maker(p, size, p, size);
+	rotate(p, size);
+	hole_maker(p, size, p, size);
+	rotate(p, size);
+	hole_maker(p, size, p, size);
 
-    for (j = 0; j < size; j++)
-    {
-        for (i = 0; i < size; i++)
-            printf("%c", p[j][i]);
-        printf("\n");
-    }
-    finallyfree(p, size);
+	for (j = 0; j < size; j++)
+	{
+		for (i = 0; i < size; i++)
+			printf("%c", p[j][i]);
+		printf("\n");
+	}
+	finallyfree(p, size);
 }
