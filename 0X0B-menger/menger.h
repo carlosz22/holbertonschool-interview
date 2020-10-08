a4d0ee43274b496272c54e0e45eae35c4ebae9ae
Carlos Zuluaga <carlosez9@hotmail.com>
2020-10-07 23:16:01 -0500

    Add exercise

---
+++ 0X0B-menger/menger.h
@@ -0,0 +1,12 @@
+#ifndef MENGER_H_
+#define MENGER_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/* Function definitions */
+
+void menger(int level);
+
+#endif /* MENGER_H_ */
